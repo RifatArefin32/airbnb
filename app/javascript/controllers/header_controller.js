57bdbaa7d076805060aec2c7a8a37e8e90462251
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-10-15 14:52:47 +0600

    Add Javascript controller  to perform toggle operations at user profile

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+import { enter, leave, toggle } from "el-transition"
+
+
+export default class extends Controller {
+
+  static targets = ['openUserMenu', 'dropdown', ]
+
+  connect() {
+    console.log("Header connected");
+    console.log();
+
+    this.openUserMenuTarget.addEventListener("click", (e) => {
+      console.log("Header Click");
+      this.toggleDropdown(this.dropdownTarget);
+    })
+  }
+
+  toggleDropdown(element) {
+    toggle(element).then(() => {
+      console.log("Enter transition complete");
+    })
+  }
+
+  closeDropdown() {
+    leave(element).then(() => {
+      element.destroy();
+    })
+  }
+}
