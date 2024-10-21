c17007cacf297c4f830356ba3923aac08efa0eca
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-10-21 14:33:47 +0600

    Create database for reservation, create reservation model and make relationship with user and properties

--- app/models/property.rb
+++ app/models/property.rb
@@ -11,9 +11,14 @@ class Property < ApplicationRecord
   has_many_attached :images
 
   has_many :feedbacks, dependent: :destroy
+
   has_many :wishlists, dependent: :destroy
   has_many :wishlisted_users, through: :wishlist, source: :user, dependent: :destroy
 
+  has_many :reservations, dependents: :destroy
+  has_many :reserved_users, through: :reservations, source: :user, dependent: :destroy
+
+  
   def average_rating
     average = feedbacks.average(:rating) || 0
     average.round(1)
